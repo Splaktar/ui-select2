5fb6a48cd8320e0acaf7b1849ef8365d7f1dfcf4
mprent99 <mprent99@harris.com>
2014-11-12 17:34:34 -0500

    Setup custom Chrome environment when running in travis-ci.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,15 +2,13 @@
 // Generated on Tue Sep 17 2013 06:59:46 GMT-0400 (EDT)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path, that will be used to resolve files and exclude
     basePath: '..',
 
-
     // frameworks to use
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -27,34 +25,25 @@ module.exports = function(config) {
       'test/*Spec.js'
     ],
 
-
     // list of files to exclude
-    exclude: [
-      
-    ],
-
+    exclude: [],
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // Start these browsers, currently available:
     // - Chrome
@@ -66,15 +55,24 @@ module.exports = function(config) {
     // - IE (only Windows)
     browsers: ['Chrome', 'Firefox'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
 
-
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
     singleRun: false
+  };
 
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
 
-  });
+  config.set(configuration);
 };
